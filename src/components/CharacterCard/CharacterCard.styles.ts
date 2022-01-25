7cf0674cf9555dca0b7b46fe23ace17a8d0ccfb3
jugalkpatel <ptjimmy99@gmail.com>
2022-01-25 13:17:02 +0530

    feat: add CharacterCard style components

--- src/components/CharacterCard/CharacterCard.styles.ts
+++ src/components/CharacterCard/CharacterCard.styles.ts
@@ -1,6 +1,62 @@
 import styled from "styled-components";
+import { borderRadius } from "../../styles/common.styles";
 const CardContainer = styled.div`
   color: ${({ theme }) => theme.primary};
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 3fr 1fr 1fr;
+  font-family: "Rubik";
+  color: ${({ theme }) => theme.secondary};
+  border-radius: ${borderRadius};
+  position: relative;
+  cursor: pointer;
+  height: 25rem;
 `;
 
-export { CardContainer };
+const ImgContainer = styled.article`
+  grid-row: 1 / -1;
+  grid-column: 1 / -1;
+`;
+
+const CharacterImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: top;
+  border-radius: ${borderRadius};
+`;
+
+const TextContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  grid-column: 1 / -1;
+  grid-row: 3 / 4;
+  align-self: center;
+  padding: 1rem;
+`;
+
+const NameText = styled.h3`
+  margin-bottom: 0.3rem;
+`;
+
+const ActorText = styled.h1``;
+
+const FavButton = styled.button`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  background-color: transparent;
+  color: ${({ theme }) => theme.favicon};
+  border-color: transparent;
+  font-size: 2rem;
+`;
+
+export {
+  CardContainer,
+  CharacterImg,
+  TextContainer,
+  NameText,
+  ActorText,
+  ImgContainer,
+  FavButton,
+};
