98005a5ce3b32039ff29fa23de8bf727322c54b4
jugalkpatel <ptjimmy99@gmail.com>
2022-01-25 13:10:28 +0530

    feat: add borderRadius to common styles

--- src/styles/common.styles.ts
+++ src/styles/common.styles.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
+const borderRadius = "1rem";
+
 const MaxWidthContainer = styled.div`
   max-width: 1280px;
   margin: 0 auto;
 `;
 
-export { MaxWidthContainer };
+export { MaxWidthContainer, borderRadius };
