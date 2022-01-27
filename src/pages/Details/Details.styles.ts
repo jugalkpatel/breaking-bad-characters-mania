a0d51ef0cf03c35aa99ca9d8357ebc00ebf687bc
jugalkpatel <ptjimmy99@gmail.com>
2022-01-27 16:17:27 +0530

    refactor: add AppContext to Details page

--- src/pages/Details/Details.styles.ts
+++ src/pages/Details/Details.styles.ts
@@ -1,8 +1,192 @@
 import styled from "styled-components";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { CgClose } from "react-icons/cg";
+import { VscPerson } from "react-icons/vsc";
+import { RiCake2Fill } from "react-icons/ri";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+import { GiTombstone } from "react-icons/gi";
+
+import { CharacterImg, MaxWidthContainer } from "../../styles/common.styles";
+
+const WidthContainer = styled(MaxWidthContainer)`
+  max-width: 991px;
+`;
 
 const DetailsContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  padding: 1rem;
+  gap: 1rem;
+
+  @media (min-width: 568px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 `;
 
-export { DetailsContainer };
+const Heart = styled(AiOutlineHeart)`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.favicon};
+`;
+
+const HeartFill = styled(AiFillHeart)`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.favicon};
+`;
+
+const Close = styled(CgClose)`
+  font-size: 1.5rem;
+`;
+
+const CloseButton = styled.button`
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  justify-self: start;
+  padding: 0.5rem;
+  border-color: transparent;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const HeartIcon = styled.button`
+  grid-column: -1 / -2;
+  justify-self: end;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-color: transparent;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Title = styled.h2`
+  color: ${({ theme }) => theme.black};
+`;
+
+// name
+const Name = styled.section`
+  grid-column: 1 / -1;
+`;
+
+// image
+const ImgContainer = styled.article`
+  grid-column: 1 / -1;
+  max-height: 300px;
+
+  @media (min-width: 568px) {
+    grid-column: 1 / 2;
+    grid-row: 3 / 5;
+  }
+`;
+
+const Image = styled(CharacterImg)`
+  object-fit: contain;
+`;
+
+// seasons
+const Seasons = styled.section`
+  grid-column: 1 / -1;
+
+  @media (min-width: 568px) {
+    grid-column: 2 / 4;
+  }
+`;
+
+const List = styled.article`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 0.5rem;
+`;
+
+// occupation
+const Occupation = styled.section`
+  grid-column: 1 / -1;
+`;
+
+const Label = styled.label`
+  margin: 0.5rem;
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.secondary};
+  border-radius: 0.3rem;
+  padding: 0.5rem 1rem;
+`;
+
+const TileContainer = styled.section`
+  grid-column: 1 / -1;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0.5rem;
+
+  @media (min-width: 568px) {
+    grid-column: 2 / 4;
+  }
+`;
+
+const Tile = styled.article`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.secondary};
+  padding: 1rem;
+  border-radius: 0.7rem;
+`;
+
+const TileLabel = styled.label`
+  display: block;
+  font-weight: 900;
+  font-size: 1.3rem;
+`;
+
+const TileContent = styled.label`
+  font-size: 1rem;
+  font-weight: 500;
+`;
+
+const Actor = styled(VscPerson)`
+  font-size: 2rem;
+`;
+const BirthDayCake = styled(RiCake2Fill)`
+  font-size: 2rem;
+`;
+const NickName = styled(MdOutlineDriveFileRenameOutline)`
+  font-size: 2rem;
+`;
+const TombStone = styled(GiTombstone)`
+  font-size: 2rem;
+`;
+
+const Line = styled.hr`
+  grid-column: 1 / -1;
+  color: ${({ theme }) => theme.border};
+  margin: 0.5rem 0;
+`;
+
+export {
+  ImgContainer,
+  DetailsContainer,
+  CloseButton,
+  HeartIcon,
+  Heart,
+  Close,
+  Name,
+  Seasons,
+  Label,
+  Tile,
+  TileContainer,
+  TileLabel,
+  Actor,
+  BirthDayCake,
+  NickName,
+  TombStone,
+  TileContent,
+  Line,
+  Image,
+  List,
+  Title,
+  Occupation,
+  WidthContainer,
+  HeartFill,
+};
--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,14 +1,133 @@
-import { useLocation } from "react-router-dom";
-import { Character } from "../../hooks/useGetCharacters.hook";
+import { useNavigate, useParams } from "react-router-dom";
+
+import {
+  Close,
+  Heart,
+  CloseButton,
+  DetailsContainer,
+  HeartIcon,
+  ImgContainer,
+  Name,
+  Seasons,
+  Label,
+  TileContainer,
+  Tile,
+  Actor,
+  BirthDayCake,
+  NickName,
+  TombStone,
+  TileLabel,
+  TileContent,
+  Line,
+  Image,
+  List,
+  Title,
+  Occupation,
+  WidthContainer,
+  HeartFill,
+} from "./Details.styles";
+
+import { Character } from "../../common.types";
+import { useAppContext } from "../../contexts";
+import { isCharacterInFavorites } from "../../utils";
 
 function Details() {
-  const location = useLocation();
+  const { id } = useParams();
+  const charID = parseInt(id as string);
+  const navigate = useNavigate();
+  const { characters, dispatch, favorites } = useAppContext();
+  const details = characters.find(
+    ({ char_id }) => char_id === charID
+  ) as Character;
 
-  const state = location.state as Character;
+  const isCharInFavorites = isCharacterInFavorites(
+    favorites,
+    details.char_id
+  ) ? (
+    <HeartFill />
+  ) : (
+    <Heart />
+  );
 
-  console.log({ state });
+  return (
+    <WidthContainer>
+      <DetailsContainer>
+        <CloseButton onClick={() => navigate("/")}>
+          <Close />
+        </CloseButton>
+        <HeartIcon
+          onClick={() =>
+            dispatch({ type: "SET_FAVORITE", payload: details.char_id })
+          }
+        >
+          {isCharInFavorites}
+        </HeartIcon>
 
-  return <h1>This is Details</h1>;
+        <Name>
+          <Title>{details.name}</Title>
+          <Line />
+        </Name>
+
+        <ImgContainer>
+          <Image src={details.img} />
+        </ImgContainer>
+
+        <Seasons>
+          <Title>Seasons</Title>
+          <Line />
+          <List>
+            {details.appearance.map((season) => (
+              <Label key={season}>{season}</Label>
+            ))}
+          </List>
+        </Seasons>
+
+        <TileContainer>
+          <Tile>
+            <Actor />
+            <div>
+              <TileLabel>Actor</TileLabel>
+              <TileContent>{details.portrayed}</TileContent>
+            </div>
+          </Tile>
+
+          <Tile>
+            <BirthDayCake />
+            <div>
+              <TileLabel>Birthday</TileLabel>
+              <TileContent>{details.birthday}</TileContent>
+            </div>
+          </Tile>
+
+          <Tile>
+            <NickName />
+            <div>
+              <TileLabel>NickName</TileLabel>
+              <TileContent>{details.nickname}</TileContent>
+            </div>
+          </Tile>
+
+          <Tile>
+            <TombStone />
+            <div>
+              <TileLabel>Alive ?</TileLabel>
+              <TileContent>{details.status}</TileContent>
+            </div>
+          </Tile>
+        </TileContainer>
+
+        <Occupation>
+          <Title>Occupation</Title>
+          <Line />
+          <List>
+            {details.occupation.map((occupation, index) => {
+              return <Label key={index}>{occupation}</Label>;
+            })}
+          </List>
+        </Occupation>
+      </DetailsContainer>
+    </WidthContainer>
+  );
 }
 
 export { Details };
