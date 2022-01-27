c57b8863e091f4e0ecbe2e74cd949bcd81ff6e52
jugalkpatel <ptjimmy99@gmail.com>
2022-01-27 16:06:24 +0530

    feat: add common styles

--- src/styles/common.styles.ts
+++ src/styles/common.styles.ts
@@ -7,4 +7,46 @@ const MaxWidthContainer = styled.div`
   margin: 0 auto;
 `;
 
-export { MaxWidthContainer, borderRadius };
+const CharacterImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: top;
+  border-radius: ${borderRadius};
+`;
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
+  padding: 1rem;
+  gap: 1rem;
+  align-items: center;
+
+  @media (min-width: 568px) {
+    grid-template-columns: repeat(auto-fill, minmax(15.4rem, 15.4rem));
+    justify-content: center;
+  }
+`;
+
+const Message = styled.h2`
+  color: ${({ theme }) => theme.primary};
+  text-align: center;
+  grid-column: 1 / -1;
+`;
+
+const HeightContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+`;
+
+export {
+  MaxWidthContainer,
+  borderRadius,
+  CharacterImg,
+  Main,
+  Message,
+  HeightContainer,
+};
